6ba29b323887dddd004977179ed669587633c071
sroyal-jpg <sroyal@ucsb.edu>
2026-05-21 18:57:47 -0700

    trying to push something again

---
+++ scripts/test_file.R
@@ -0,0 +1,11 @@
+~/tmp/ESM262_Royal_Repo % git remote -v
+
+git remote add origin https://github.com/sroyal-jpg/ESM262_Royal_Repo.git
+
+
+git clone https://PAT@github.com/sroyal-jpg/ESM262_Royal_Repo.git
+
+
+git remote set-url origin new_remote-url
+
+now trying to push something
